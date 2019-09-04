c19e98ddebf1d161d556a68a2761173b7e8d90b4
Jamie Barmakian <jamie.barmakian@gmail.com>
2019-09-03 20:17:51 -0400

    firstCommit

---
+++ data.js
@@ -0,0 +1,130 @@
+var questionData = [
+  {
+    questionText: 'Which is the largest National Park?',
+    answerText: [
+        'Wrangell-St. Elias National Park',
+        'Grand Canyon National Park',
+        'Yosemite National Park',
+        'Grand Teton National Park'
+    ],
+    correctAnswerIndex: 0,
+    correctAnswerText: 'Wrangell-St. Elias National Park is the largest National Park.',
+    answerImg: "Photos/Wrangell-St-Elias-Q2.jpg"
+  },
+  {
+    questionText: 'How many National Parks are in the state of Colorado?',
+    answerText: [
+        'Four',
+        'One',
+        'Five',
+        'Two',
+    ],
+    correctAnswerIndex: 0,
+    correctAnswerText: 'Four: The Rocky Mountains, The Great Sand Dunes, Mesa Verde, and The Black Canyon of Gunnison.',
+    answerImg: "Photos/sand-dunes-Q1.jpg"
+
+  },
+  {
+    questionText: 'Which National Park was NOT founded by Theodore Roosevelt?',
+    answerText: [
+        'Mesa Verde National Park',
+        'Crater Lake National Park',
+        'Wind Cave National Park',
+        'Shenandoah National Park',
+    ],
+    correctAnswerIndex: 3,
+    correctAnswerText: 'Theodore Roosevelt signed legislation establishing five national parks: Crater Lake, Wind Cave, Sullys Hill (later re-designated a game preserve), Mesa Verde, and Platt(now part of Chickasaw National Recreation Area).',
+    answerImg: "Photos/wind-cave-Q3.jpg"
+
+  },
+  {
+    questionText: 'Which National Park is NOT a part of the Pacific Crest Trail?',
+    answerText: [
+        'Yosemite National Park',
+        'North Cascades National Park',
+        'Kings Canyon National Park',
+        'Crater Lake National Park',
+    ],
+    correctAnswerIndex: 3,
+    correctAnswerText: 'Crater Lake National Park is not a part of the PCT.',
+    answerImg: "Photos/PCT-Q4.jpg"
+
+  },
+  {
+    questionText: 'Around how many arches are in Arches National Park?',
+    answerText: [
+        '500',
+        '2,000',
+        '1,000',
+        '4,000',
+    ],
+    correctAnswerIndex: 1,
+    correctAnswerText: '2,000 arches can be found in Arches National Park.',
+    answerImg: "Photos/arches-Q5.jpg"
+
+  },
+  {
+    questionText: 'How many National Parks are in the United States?',
+    answerText: [
+        '35',
+        '68',
+        '58',
+        '47',
+    ],
+    correctAnswerIndex: 2,
+    correctAnswerText: 'There are 58 National Parks in the US.',
+    answerImg: "Photos/58-Q6.jpg"
+  },
+  {
+    questionText: 'Which National Park was inspired by sisters Esther and Elizabeth Burnell, becoming the first female naturalists certified by the National Park Service?',
+    answerText: [
+        'Everglades National Park',
+        'Hot Springs National Park',
+        'Joshua Tree National Park',
+        'Rocky Mountains National Park'
+    ],
+    correctAnswerIndex: 3,
+    correctAnswerText: 'The Rocky Mountains National Park',
+    answerImg: "Photos/rocky-mountains-Q7.jpg"
+
+  },
+  {
+    questionText: 'Which is the most visited National Park?',
+    answerText: [
+        'The Great Smoky Mountains National Park',
+        'The Grand Canyon National Park',
+        'Yosemite National Park',
+        'Glacier National Park',
+    ],
+    correctAnswerIndex: 0,
+    correctAnswerText: 'The Great Smoky Mountains: 11.3 million people visit The Great Smoky Mountains each year, followed by The Grand Canyon at an estimated 5.9 million people.',
+    answerImg: "Photos/great-smokys-Q8.jpg"
+
+  },
+  {
+    questionText: 'Which was the first ever National Park?',
+    answerText: [
+        'Yellowstone National Park',
+        'Acadia National Park',
+        'Zion National Park',
+        'Mount Rainer National Park',
+    ],
+    correctAnswerIndex: 0,
+    correctAnswerText: 'Yellowstone was the first National Park,signed into law by President Ulysses S. Grant in 1872.',
+    answerImg: "Photos/yellowstone-Q9.jpg"
+
+  },
+  {
+    questionText: 'Which National Park was the first instance where land was donated to the federal government?',
+    answerText: [
+        'The Great Sand Dunes National Park',
+        'Acadia National Park',
+        'Cuyahoga Valley National Park',
+        'Mesa Verde National Park',
+    ],
+    correctAnswerIndex: 1,
+    correctAnswerText: 'Acadia National Park',
+    answerImg: "Photos/Acadia-Q10.jpg"
+
+  }
+]
---
+++ script.js
@@ -0,0 +1,205 @@
+/* global $, questionData */
+
+var mainController = {
+  data: { 
+    currentQuestionIndex: 0,
+    userAnswerIndex: -1,
+    score: 0,
+    questions: questionData,
+    questionCount: questionData.length,
+    answerImg: questionData.answerImg
+  },
+
+  uiElements: {
+    startButton: null,
+    startPage: null,
+    questionPage: null,
+    answerPage: null,
+    finalPage: null,
+    finalButton: null,
+    
+    questionText: null,
+    answerInput1: null,
+    answerInput2: null,
+    answerInput3: null,
+    answerInput4: null,
+    answerText1: null,
+    answerText2: null,
+    answerText3: null,
+    answerText4: null,
+    nexttButton: null,
+    questionForm: null,
+    questionPageMessage: null,
+    answerImg: null,
+    
+    questionScoreHeader: null,
+    answerResultText: null,
+    questionNumber: null,
+    questionCount: null,
+    score: null,
+    finalPageText: null
+  },
+
+  init: function () {
+    console.log('init function called');
+    
+    // pages
+    this.uiElements.startButton = $('#start-button');
+    this.uiElements.startPage = $('#start-page');
+    this.uiElements.questionPage = $('#question-page');
+    this.uiElements.answerPage = $('#answer-page');
+    this.uiElements.finalPage = $('#final-page');
+
+    // question page elements
+    this.uiElements.questionText = $('#question-text');
+    this.uiElements.answerInput1 = $('#answer-radio-1');
+    this.uiElements.answerInput2 = $('#answer-radio-2');
+    this.uiElements.answerInput3 = $('#answer-radio-3');
+    this.uiElements.answerInput4 = $('#answer-radio-4');
+    this.uiElements.answerText1 = $('#answer-label-1');
+    this.uiElements.answerText2 = $('#answer-label-2');
+    this.uiElements.answerText3 = $('#answer-label-3');
+    this.uiElements.answerText4 = $('#answer-label-4');
+    this.uiElements.answerImg = $("#answer-img");
+
+    this.uiElements.nextButton = $('#next-button');
+
+    this.uiElements.questionPageMessage = $('#question-page-message');
+    this.uiElements.questionForm = $('#question-form');
+
+    this.uiElements.answerResultText = $("#answer-result-text");
+
+    this.uiElements.questionScoreHeader = $('#question-score-header');
+    this.uiElements.questionNumber = $('.questionNumber');
+    this.uiElements.questionCount = $('.questionCount');
+    this.uiElements.score = $('.score');
+    
+    this.uiElements.finalPageText = $('#final-page-text');
+    this.uiElements.finalButton = $('#final-button');
+    
+    this.wireEvents();
+  },
+  
+  wireEvents: function () {
+    console.log('wireEvents function called');
+    var that = this;
+
+    this.uiElements.startButton.click(function () {
+      that.uiElements.questionNumber.text('1');
+      that.uiElements.questionCount.text(that.data.currentQuestionIndex + 1);
+      that.uiElements.score.text(that.data.score);
+      that.uiElements.questionScoreHeader.show();
+      
+      that.fillInQuestionText(that);
+      that.uiElements.questionPage.show();
+      that.uiElements.startPage.hide();
+    });
+
+    this.uiElements.finalButton.click(function(){
+      that.uiElements.startPage.show();
+      that.uiElements.startPage.hide();
+    })
+    
+    this.uiElements.questionForm.submit(function(e) {
+      console.log('question form submit');
+      e.preventDefault();
+      
+      var isChecked1 = that.uiElements.answerInput1.is(':checked');
+      var isChecked2 = that.uiElements.answerInput2.is(':checked');
+      var isChecked3 = that.uiElements.answerInput3.is(':checked');
+      var isChecked4 = that.uiElements.answerInput4.is(':checked');
+      
+      if( !isChecked1 && !isChecked2 && !isChecked3 && !isChecked4 ) {
+        that.uiElements.questionPageMessage.text(
+          'You must select an answer before submitting.'
+        );
+      } else {
+        var answerIndex = document.forms['question-form']['answers'].value;
+        that.data.userAnswerIndex = answerIndex;
+        that.fillAnswerPage();
+        that.uiElements.answerPage.show();
+        that.uiElements.questionPage.hide();
+      }
+    });
+    
+    this.uiElements.nextButton.click(function() {
+      if(that.data.currentQuestionIndex == that.data.questionCount - 1) {
+        that.fillFinalPage();
+        that.uiElements.finalPage.show();
+        that.uiElements.answerPage.hide();
+      } else {
+        that.data.currentQuestionIndex += 1;
+        that.uiElements.questionNumber.text(that.data.currentQuestionIndex + 1);
+        that.fillInQuestionText(that);
+        that.uiElements.questionPage.show();
+        that.uiElements.answerPage.hide();
+      }
+    })
+  },
+  
+  fillInQuestionText: function(that) {
+    console.log('fillInQuestionText called');
+    var currQuestion = that.data.questions[that.data.currentQuestionIndex];
+    that.uiElements.questionPageMessage.text('');
+    that.uiElements.questionText.text(currQuestion.questionText);
+    that.uiElements.answerText1.text(currQuestion.answerText[0]);
+    that.uiElements.answerText2.text(currQuestion.answerText[1]);
+    that.uiElements.answerText3.text(currQuestion.answerText[2]);
+    that.uiElements.answerText4.text(currQuestion.answerText[3]);
+    
+    // uncheck all answer radio buttons
+    that.uiElements.answerInput1.prop('checked', false);
+    that.uiElements.answerInput2.prop('checked', false);
+    that.uiElements.answerInput3.prop('checked', false);
+    that.uiElements.answerInput4.prop('checked', false);
+  },
+
+  fillAnswerPage: function() {
+    var currQuestion = this.data.questions[this.data.currentQuestionIndex];
+
+    var userAnswerIndex = this.data.userAnswerIndex;
+    var userAnswerText = 'Your Answer: ';
+    userAnswerText += currQuestion.answerText[userAnswerIndex];
+
+    var correctQuestionText = 'Question: ';
+    correctQuestionText += currQuestion.questionText;
+    
+    var correctAnswerIndex = currQuestion.correctAnswerIndex;
+    var correctAnswerText = 'Correct Answer: ';
+    correctAnswerText += currQuestion.answerText[correctAnswerIndex];
+    this.uiElements.answerImg.prop("src", currQuestion.answerImg);
+    console.log(this.uiElements.answerImg.attr("src"));
+    console.log(currQuestion.answerImg);
+    if(userAnswerIndex == correctAnswerIndex) {
+      this.data.score += 1;
+      this.uiElements.score.text(this.data.score);
+      this.uiElements.answerResultText.text(
+        'Correct: ' + currQuestion.correctAnswerText
+      )
+    } else {
+      this.uiElements.answerResultText.text(
+        'Incorrect: ' + currQuestion.correctAnswerText
+      )
+    }
+  },
+  
+  fillFinalPage: function() {
+    var text;
+    
+    if(this.data.score < 4) {
+      text = 'You need to spend more time outdoors!';
+    } else if(this.data.score < 8) {
+      text = 'You know your parks!'
+    } else {
+      text = 'You\'re a National Park expert!'
+    }
+    
+    this.uiElements.finalPageText.text(text);
+  }
+
+}
+
+$(document).ready(function () {
+  mainController.init();
+});
+
